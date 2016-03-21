20d217b5027d26cb17fc809a37d1c292e5aee796
Page Piccinini <page.piccinini@gmail.com>
2016-03-21 11:07:31 +0100

    Change some terms in statistics script.

--- scripts/rcourse_lesson4_statistics.R
+++ scripts/rcourse_lesson4_statistics.R
@@ -13,37 +13,37 @@ data_stats = data_clean %>%
 
 
 ## BUILD MODELS ####
-# One predictor (series)
+# One variable (series)
 extinct_series.glm = glm(extinct ~ series, family = "binomial", data = data_stats)
 
 extinct_series.glm_sum = summary(extinct_series.glm)
 extinct_series.glm_sum
 
-# One predictor (alignment)
+# One variable (alignment)
 extinct_alignment.glm = glm(extinct ~ alignment, family = "binomial", data = data_stats)
 
 extinct_alignment.glm_sum = summary(extinct_alignment.glm)
 extinct_alignment.glm_sum
 
-# Two predictors additive
+# Two variables additive
 extinct_seriesalignment.glm = glm(extinct ~ series + alignment, family = "binomial", data = data_stats)
 
 extinct_seriesalignment.glm_sum = summary(extinct_seriesalignment.glm)
 extinct_seriesalignment.glm_sum
 
-# Two predictors interaction (pre-determined baselines)
+# Two variables interaction (pre-determined baselines)
 extinct_seriesxalignment.glm = glm(extinct ~ series * alignment, family = "binomial", data = data_stats)
 
 extinct_seriesxalignment.glm_sum = summary(extinct_seriesxalignment.glm)
 extinct_seriesxalignment.glm_sum
 
-# Two predictors interaction (change baseline for series)
+# Two variables interaction (change baseline for series)
 extinct_seriesxalignment_tng.glm = glm(extinct ~ relevel(series, "tng") * alignment, family = "binomial", data = data_stats)
 
 extinct_seriesxalignment_tng.glm_sum = summary(extinct_seriesxalignment_tng.glm)
 extinct_seriesxalignment_tng.glm_sum
 
-# Two predictors interaction (change baseline for alignment)
+# Two variables interaction (change baseline for alignment)
 extinct_seriesxalignment_friend.glm = glm(extinct ~ series * relevel(alignment, "friend"), family = "binomial", data = data_stats)
 
 extinct_seriesxalignment_friend.glm_sum = summary(extinct_seriesxalignment_friend.glm)
